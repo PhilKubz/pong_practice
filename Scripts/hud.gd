d79b3554b1a8b5d60a579682ab7fde11a15670b7
PhilKubsz <philip.kubisz@gmail.com>
2023-09-29 12:33:13 -0700

    HUD fix to show scoring

--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func set_score_player_label(new_score_player):
-	score_player.text = str(score_player) + " :  "
+	score_player.text = str(new_score_player) + " :  "
 
 func set_score_opponent_label(new_score_opponent):
-	score_opponent.text = str(score_opponent)
+	score_opponent.text = str(new_score_opponent)
